fe99fa19d30c5fe112e303785f5813444ae0db03
Anton <gendra.job@gmail.com>
2026-05-10 12:20:50 +0200

    feat: Create java api skeleton. Add profiles. Dockerize api

---
+++ api/src/main/java/com/gen_4/horse_exchange/HorseExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.gen_4.horse_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseExchangeApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/gen_4/horse_exchange/HorseExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gen_4.horse_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
